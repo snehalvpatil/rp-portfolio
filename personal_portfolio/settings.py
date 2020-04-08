8d1e9c6c10fe254538d50714af25f078d0b70a27
snehalvpatil <snehalvpatil25@gmail.com>
2020-04-08 16:02:09 +0530

    make it better

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')33zofoll#&c0it4e306mhkl=k^rk4o_!%=h1^)pcgi7ht=df^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT =  '/home/django/django_project/django_project/static'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
